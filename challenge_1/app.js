65e55fc1ece089fb120536ed390b6046fa9f813a
Chris Palka <cpalka87gmail.com>
2020-07-02 16:14:16 -0600

    Add reset button and complete algorithm for X/O Win or Tie

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,39 +1,121 @@
-// grab all divs individually
-
-// var topLeft = document.getElementById('div1');
-// var topMid = document.getElementById('div2');
-// var topRight = document.getElementById('div3');
-// var midLeft = document.getElementById('div4');
-// var midMid = document.getElementById('div5');
-// var midRight = document.getElementById('div6');
-// var botLeft = document.getElementById('div7');
-// var botMid = document.getElementById('div8');
-// var botRight = document.getElementById('div9');
-
 var turnLog = {};
 var turnArray = [];
-
+var solutionArray = [
+  [
+    'square1',
+    'square2',
+    'square3'
+  ],
+  [
+    'square4',
+    'square5',
+    'square6'
+  ],
+  [
+    'square7',
+    'square8',
+    'square9'
+  ],
+  [
+    'square1',
+    'square4',
+    'square7'
+  ],
+  [
+    'square2',
+    'square5',
+    'square8'
+  ],
+  [
+    'square3',
+    'square6',
+    'square9'
+  ],
+  [
+    'square1',
+    'square5',
+    'square9'
+  ],
+  [
+    'square3',
+    'square5',
+    'square7'
+  ]
+];
 
 var clicker = function (section) {
+
   // section.innerHTML = `<img src=${'./assets/o-transparent.png'} width='12%' height='25%'></img>`
   // console.log(section.getElementById('div1'))
-
+  /* <img src=${'./assets/o-transparent.png'} width='7%' height='25%'></img> */
   var currentSquare = section.id;
   if (!turnLog.hasOwnProperty(currentSquare)) {
-    turnLog[currentSquare] = currentSquare;
     var lastTurn = turnArray.pop()
     if (lastTurn === 'O' || lastTurn === undefined) {
-      section.innerHTML = '<p>X</p>';
+      section.innerHTML = `<p>X</p>`;
+      turnLog[currentSquare] = 'X';
       turnArray.push('X');
     } else {
-      section.innerHTML = '<p>O</p>';
+      section.innerHTML = `<p>O</p>`;
+      turnLog[currentSquare] = 'O';
       turnArray.push('O');
     }
   } else {
     alert('choose another spot!')
   }
+  solutionChecker();
+  console.log(turnLog)
   // section.setAttribute('id', `${section.getAttribute('id')} inactive`);
   // // section.innerHTML = `<img src=${'./assets/o-transparent.png'} width='12%' height='25%'></img>`
+};
+
+
+function solutionChecker() {
+  var solutionCompiler = [];
+  // if (!solutionCompiler.includes(undefined)) {
+    //   console.log('tie')
+    // }
+    for (var i = 0; i < solutionArray.length; i++) {
+      var solutions = solutionArray[i];
+      for (var s = 0; s < solutions.length; s++) {
+        solutionCompiler.push(turnLog[solutions[s]]);
+      }
+    }
+    var sliced = solutionSlicer(solutionCompiler)
+    for (var x = 0; x < sliced.length; x++) {
+      if (sliced[x].join('') === 'XXX') {
+        alert('X WINS!!');
+        clearGame();
+        return;
+      } else if (sliced[x].join('') === 'OOO') {
+        alert('O WINS!!');
+        clearGame();
+        return;
+      }
+    }
+    if (!solutionCompiler.includes(undefined)) {
+      alert('tie');
+      clearGame();
+    }
 
 }
 
+
+function solutionSlicer(array) {
+  var result = [];
+  for (var i = 0; i < array.length; i = i + 3) {
+    var sliceIt = array.slice(i, i + 3)
+    result.push(sliceIt)
+  }
+  return result;
+}
+
+function clearGame() {
+  var board = document.getElementById('board');
+  var nodeList = board.childNodes;
+  nodeList.forEach(function(node) {
+    node.innerHTML = ''
+  });
+  turnLog = {}
+  turnArray = [];
+}
