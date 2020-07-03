de56c014f35f9cca836e78d71545d2b26512098c
Chris Palka <cpalka87gmail.com>
2020-07-03 15:45:16 -0600

    Serve index.html via express when resolving '/' route

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './client/index.html'))
+)
 
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
