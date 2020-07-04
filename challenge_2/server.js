4d4c2d80067452174576a94b59c5106e881ffb9a
Chris Palka <cpalka87gmail.com>
2020-07-04 01:15:47 -0600

    Receive response from server after client submit

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,8 +3,20 @@ const app = express()
 const port = 3000
 const path = require('path');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './client/index.html'))
 )
+
+app.post('/submit', (req, res, next) => {
+  console.log('REQUEST', req.body.submittext)
+  var data = req.body.submittext;
+  // res.write(data, 'HI')
+  res.end()
+  // console.log('RESPONSE', res);
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
