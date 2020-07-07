2c3a96a4a2909f01e2d37ee30c849eba4eb9f577
Chris Palka <cpalka87gmail.com>
2020-07-07 01:50:00 -0600

    Begin implementation of file upload

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const fs = require('fs');
 const csvParser = require('./model/csvParse');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,18 +17,27 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.post('/submit', (req, res, next) => {
-  var data = JSON.parse(req.body.submittext);
-  // res.write(data, 'HI')
-  data = csvParser(data);
-  // res.render('index', { data: data }, function (err, html) {
-  //   if (err) {
-  //     console.log('ERROR! ', err)
-  //   } else {
-  //     res.send(html);
-  //   }
-  // })
-  res.render('submit', { data })
+app.post('/submit', upload.single('json-report'), (req, res, next) => {
+  console.log(req.file)
+  var file = req.file.filename;
+  fs.readFile(path.join(__dirname, `uploads/${file}`), 'utf8', function(err, data) {
+    if (err) {
+      console.log('Error reading file: ', err);
+    } else {
+      // data = JSON.parse(data);
+      console.log(data);
+      // csvParser(data);
+      // res.render('submit', { data });
+    }
+  });
+
+  // var data = JSON.parse(req.file);
+
+    // data = csvParser(data);
+    // console.log(data)
+    // res.render('submit', { data })
+
+  next();
 });
 
 app.listen(port, () => console.log(`CSV-Report-Tool listening at http://localhost:${port}`));
