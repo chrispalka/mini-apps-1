50a755401ab0c2d85834d7ddf5e6f25f41ee87bd
Chris Palka <cpalka87gmail.com>
2020-07-08 01:46:27 -0600

    Implement ajax file uploader - save files in local storage - utilize underscore's _.uniqueid to generate unique filenames - added cors file to alleviate header issues

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,4 +1,27 @@
-// The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report.
-// You may assume the JSON data has a regular structure and hierarchy (see included sample file). In other words, all sibling records at a particular level of the hierarchy will have the same set of properties, but child objects might not contain the same properties. In all cases, every property you encounter must be present in the final CSV output.
-// You may also assume that child records in the JSON will always be in a property called `children`.
+const server = 'http://127.0.0.1:3000';
 
+
+
+const fileUpload = (file) => {
+  var formData = new FormData();
+  formData.append('file', file);
+  $.ajax({
+    type: 'POST',
+    data: formData,
+    url: `${server}/submit`,
+    cache: false,
+    contentType: false,
+    processData: false,
+    success: () => {
+      console.log('Success!')
+      // window.location = window.location.href;
+    }
+  });
+};
+
+$('form').on('submit', function(e) {
+  e.preventDefault()
+  var form = $('form #file')[0];
+  var file = form.files[0]
+  fileUpload(file)
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,33 +3,45 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const fs = require('fs');
+const headers = require('./cors')
 const csvParser = require('./model/csvParse');
-var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' })
+const _ = require('underscore')
+
 
 app.use(express.urlencoded({ extended: true }));
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.post('/submit', upload.single('json-report'), (req, res, next) => {
-  console.log(req.file)
-  var file = req.file.filename;
-  fs.readFile(path.join(__dirname, `uploads/${file}`), 'utf8', function(err, data) {
-    if (err) {
-      console.log('Error reading file: ', err);
-    } else {
-      // data = JSON.parse(data);
-      console.log(data);
-      // csvParser(data);
-      // res.render('submit', { data });
-    }
+app.post('/submit', (req, res, next) => {
+  var data = Buffer.alloc(0);
+
+  req.on('data', (chunk) => {
+    data = Buffer.concat([data, chunk]);
+  })
+  req.on('end', () => {
+    // data = data.toString('utf8');
+    fs.writeFile(path.join(__dirname, `/uploads/${_.uniqueId('json_')}.csv`), data, function(err) {
+      if (err) {
+        console.log('Error! ', err);
+      } else {
+        console.log('File written successfully');
+      }
+    });
+    res.set(headers)
+    // var final = Buffer.from(data, 'base64').toString('utf8');
+    // console.log(final)
+    res.render('submit', { data });
   });
+  // console.log('data: ', file)
+  // data = csvParser(data)
+  // res.render('submit', { data });
 
   // var data = JSON.parse(req.file);
 
@@ -37,7 +49,7 @@ app.post('/submit', upload.single('json-report'), (req, res, next) => {
     // console.log(data)
     // res.render('submit', { data })
 
-  next();
+
 });
 
 app.listen(port, () => console.log(`CSV-Report-Tool listening at http://localhost:${port}`));
