b15228c6a61285f3c5177b120e66b82554345af5
Chris Palka <cpalka87gmail.com>
2020-07-08 20:59:06 -0600

    Spin up express server and serve index html/assets

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 
